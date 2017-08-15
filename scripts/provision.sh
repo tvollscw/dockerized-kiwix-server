a0473d7ef1149bda1f2f4c7e789880e56e4af805
Jonathan Boiser <jboiser@gmail.com>
2017-08-15 17:02:55 -0400

    Add scripts

---
+++ scripts/provision.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install kiwix-tools
+wget --quiet https://ftp.fau.de/kiwix/nightly/2017-08-13/kiwix-tools_linux64_2017-08-13.tar.gz
+tar -xvzf kiwix-tools_linux64_2017-08-13.tar.gz -C ./bin
